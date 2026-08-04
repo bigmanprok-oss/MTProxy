932d8141e8aeab3b473e56293f4ced80b3124b91
levlam <levlam@telegram.org>
2026-08-04 17:21:21 +0300

    Silently truncate process identifier.

--- common/pid.c
+++ common/pid.c
@@ -38,9 +38,7 @@ npid_t PID;
 
 void init_common_PID (void) {
   if (!PID.pid) {
-    int p = getpid ();
-    assert (!(p & 0xffff0000));
-    PID.pid = p;
+    PID.pid = (unsigned short) getpid ();
   }
   if (!PID.utime) {
     PID.utime = time (0);
